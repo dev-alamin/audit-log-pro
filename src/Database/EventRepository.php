d1df729f374a3c6f01807283041eb2e3b3f3000c
Al Amin <hmalaminmb4@gmail.com>
2026-07-12 20:38:26 +0600

    RM Query & Replaced by Repo, insert method

---
+++ src/Database/EventRepository.php
@@ -0,0 +1,57 @@
+<?php
+namespace Amin\AuditLogPro\Database;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Logger Repository
+ * 
+ * Plugin's event reposity for handling global
+ * insertion, query for rest or anything, anywhere
+ * 
+ * @since 1.0.0
+ * @author Al Amin <hmalaminmb4@gmail.com>
+ * @package AuditLogPro
+ */
+class EventRepository {
+
+	/**
+	 * Insert Logs
+	 *
+	 * Plugin's main log inserter method
+	 *
+	 * @param array $data
+	 * @return boolean
+	 */
+	public function insert( array $data ): bool {
+
+		if ( count( $data ) != 7 ) {
+			return false; // We expect 7 items as per formatting
+		}
+
+		global $wpdb;
+		$table = $wpdb->prefix . ADTLOGPRO_TABLE_NAME;
+
+		$inserted = $wpdb->insert(
+			$table,
+			$data,
+			array(
+				'%s',
+				'%d',
+				'%s',
+				'%d',
+				'%s',
+				'%s',
+				'%s',
+			)
+		);
+
+		if ( $inserted ) {
+			wp_cache_delete( 'adtlogpro_dashboard_summary', 'adtlogpro' );
+		}
+
+		return (bool) $inserted;
+	}
+}
